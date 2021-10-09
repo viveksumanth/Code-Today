209527862bfdd17903f153c3d3dc6746092fc487
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-10-08 22:55:08 -0400

    Time: 636 ms (5.01%), Space: 14.2 MB (76.28%) - LeetHub

--- combination-sum/combination-sum.py
+++ combination-sum/combination-sum.py
@@ -1,24 +1,24 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        def dfs(candidates, target, subsolution, solutions):
+        def dfs(start,candidates, target, subsolution, solutions):
             # print(subsolution)
             if sum(subsolution) == target:
                 answer = sorted(subsolution)
                 if answer not in solutions:
                     solutions.append(answer)
             
-            for i in range(0,len(candidates)):
+            for i in range(start,len(candidates)):
                 subsolution.append(candidates[i])
                 
                 if sum(subsolution) <= target:
-                    dfs(candidates, target, subsolution,solutions)
+                    dfs(start,candidates, target, subsolution,solutions)
                 
                 subsolution.pop()
             
             return solutions
         
-        return (dfs(candidates, target, [], []))
+        return (dfs(0,candidates, target, [], []))
         
     
     
