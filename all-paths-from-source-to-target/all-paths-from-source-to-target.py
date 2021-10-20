ddef47751608c0fc8187460e8be5def4496abab7
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-10-20 12:33:50 -0400

    Time: 102 ms (59.34%), Space: 15.6 MB (75.56%) - LeetHub

--- all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -1,28 +1,24 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         result = []
-        visited = [False]*len(graph)
         curr = [0]
         start = 0
         target = len(graph) - 1
         
-        def dfs(start,result,visited, subpath,target):
-            
+        def dfs(start,result,subpath,target):
+
             if start == target:
                 result.append(subpath[:])
             
             for i in graph[start]:
-                
-                if visited[i] == False:
-                    visited[i] = True
-                    subpath.append(i)
-                    dfs(i, result, visited, subpath, target)
-                    subpath.pop()
-                    visited[i] = False
-            
+
+                subpath.append(i)
+                dfs(i, result, subpath, target)
+                subpath.pop()
+
             return result
         
-        return dfs(0, result, visited, curr, target)
+        return dfs(0, result, curr, target)
         
         
                 
