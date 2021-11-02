8e14cca332d269ba05bdd76874d1a570e3f89ed1
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-11-01 20:57:55 -0400

    Time: 2864 ms (5.00%), Space: 19.9 MB (7.06%) - LeetHub

--- kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,12 +1,45 @@
+# quick select
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        for i in range(len(nums)):
-            nums[i] = nums[i]*(-1)
-        heapq.heapify(nums)
-
-        for i in range(k-1):
-            heapq.heappop(nums)
-    
+    def swap(self, i, j, nums):
+        nums[i], nums[j] = nums[j], nums[i]
         
-        return heapq.heappop(nums)*(-1)
+    def quickselect(self, startIdx, endIdx, nums,k):
+        pivotIdx = startIdx
+        leftIdx =  startIdx + 1
+        rightIdx = endIdx
+        
+        while(leftIdx <= rightIdx):
+            
+            if nums[leftIdx] > nums[pivotIdx] and nums[rightIdx] < nums[pivotIdx]:
+                self.swap(leftIdx, rightIdx, nums)
+            
+            if nums[leftIdx] <= nums[pivotIdx]:
+                leftIdx += 1
+            
+            if nums[rightIdx] >= nums[pivotIdx]:
+                rightIdx -= 1
+            
+        self.swap(rightIdx, pivotIdx, nums)
+        
+        posistion = len(nums) - k
+
+        if rightIdx == posistion:
+            return 
+        
+        if posistion < rightIdx:
+            #move left from posistion
+            self.quickselect(startIdx, rightIdx - 1, nums,k)
+            
+        if posistion > rightIdx:
+            #move right
+            self.quickselect(rightIdx + 1, endIdx, nums,k)
+        
+    
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        
+        kthLargest = self.quickselect(0, len(nums) - 1, nums,k)
+
+        return nums[len(nums) - k]
+        
+        
         
