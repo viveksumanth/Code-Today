0d132b1366f91705b479e84a91471e86ccb8c832
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-11-06 19:59:43 -0400

    Time: 32 ms (95.06%), Space: 14.5 MB (25.25%) - LeetHub

--- word-break/word-break.py
+++ word-break/word-break.py
@@ -1,28 +1,44 @@
+'''
+        
+                leetcode         ["leet","code"]
+
+            leet / \ code
+                    x
+              code
+
+         leet / \ code
+             x    ''
+
+'''
 class Solution:
+            
+        
+    def DFS(self, s, wordDict, currentIdx, memo):
+        
+        currentString = s[currentIdx::]
+        
+        if currentIdx == len(s):
+            return True
+        
+        if currentString in memo:
+            return memo[currentString]
+
+        flag = False
+        for word in wordDict:
+            
+            if currentString.startswith(word): 
+                if self.DFS(s, wordDict, currentIdx + len(word), memo):
+                    memo[currentString] = True
+                    flag = True
+                    break
+        
+        memo[currentString] = flag
+        return flag
+
+        
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
-        seen = {}
-        def dfs(wordDict, s, i):
-            
-            if i == len(s):
-                return True
-            if s[i::] in seen:
-                return seen[s[i::]]
-            
-            flag = False
-            
-            for eachWord in wordDict:
-                if s[i:].startswith(eachWord):
-                    
-                    answer = dfs(wordDict, s, i+len(eachWord))
-    
-                    if answer:
-                        flag = True
-                        break
+        return self.DFS(s, wordDict, 0, {})
 
-                
-            seen[s[i::]] = flag
-                
-            return flag
+    
         
-        return dfs(wordDict, s, 0)
