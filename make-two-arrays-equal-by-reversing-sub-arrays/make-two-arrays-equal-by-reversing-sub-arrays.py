7cde01217d2dd87c8853027449dc93fd149c9400
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-11-21 10:25:31 -0500

    Time: 68 ms (98.43%), Space: 14.6 MB (31.78%) - LeetHub

--- make-two-arrays-equal-by-reversing-sub-arrays/make-two-arrays-equal-by-reversing-sub-arrays.py
+++ make-two-arrays-equal-by-reversing-sub-arrays/make-two-arrays-equal-by-reversing-sub-arrays.py
@@ -1,10 +1,14 @@
+from collections import Counter
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        hm = Counter(arr)
         
-        target.sort()
-        arr.sort()
-        
-        if target == arr:
-            return True
-        else:
-            return False
+        for i in target:
+            if i not in hm:
+                return False
+            else:
+                hm[i] -= 1
+                if hm[i] == 0:
+                    del hm[i]
+        return True
+                
