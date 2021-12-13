bf8e1427215a1e08e733cc6e7f8187602e8a5f30
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-12-13 12:48:52 -0500

    Time: 40 ms (93.10%), Space: 14.4 MB (72.10%) - LeetHub

--- find-peak-element/find-peak-element.py
+++ find-peak-element/find-peak-element.py
@@ -1,8 +1,8 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         '''
-            1 1 1 2 3 1 
-                  l r
+               |
+        [1,2,1,3,5,6,4]
             
         '''
         leftIdx = 0
@@ -12,11 +12,11 @@ class Solution:
 
             middle = (leftIdx + rightIdx)//2
             
-            if nums[middle] > nums[middle + 1]:
-                rightIdx = middle
+            if nums[middle] < nums[middle + 1]:
+                leftIdx = middle + 1
             
             else:
-                leftIdx = middle + 1
+                rightIdx = middle
         
         return leftIdx
         
