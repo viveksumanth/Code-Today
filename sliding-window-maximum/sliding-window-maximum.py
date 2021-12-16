36fb0d2a2115296c8ba770a0f8776d32260a963d
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-12-16 12:30:06 -0500

    Time: 3956 ms (5.01%), Space: 29.7 MB (82.77%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.py
+++ sliding-window-maximum/sliding-window-maximum.py
@@ -1,34 +1,38 @@
 
 class Solution:
-    def maxSlidingWindow(self, nums, k):
-        '''
-        using monotonic decreasing queue and sliding window
-
-        -> pop elements which are out of sliding window
-        -> add elements into the deque and maintain monotonic decreasing property
-
-        '''
-    
-        queue = collections.deque()
-        maxArray = []
-        pointerA = 0
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        
         pointerB = 0
-
-        for i in range(0,len(nums)):
-            # print(queue)
-            while(queue and queue[-1][0] < nums[i]):
-                queue.pop()
+        stack = []
+        maxmArray = []
+        
+        # k == 1 which means that at the window of size 1 k should be 1
+        if k == 1:
+            return nums
+        
+        for pointerA in range(0,len(nums)):
             
-            queue.append([nums[i],i])
+            # maintaing window of size k
+            while(k == 0):
+                maxmArray.append(nums[stack[0]])
+                pointerB = pointerB + 1
+                k += 1
             
-            if pointerA > queue[0][1]:
-                queue.popleft()
+            # maintaing monotonocity
+            while(stack and nums[stack[-1]] < nums[pointerA]):
+                stack.pop()
             
-            if (i + 1) >= k:
-                maxArray.append(queue[0][0])
-                pointerA += 1  
-        return(maxArray)
-
+            # removing the element which is out of bound 
+            if stack and stack[0] < pointerB:
+                    stack.pop(0)
+            
+            # adding new element
+            stack.append(pointerA)
+            k = k - 1
+            
+        # for the last window
+        maxmArray.append(nums[stack[0]])
+        return maxmArray
 
             
         
