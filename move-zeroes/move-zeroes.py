d7d075e972399d7144807074c3035fa87e9edfef
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-12-23 23:09:22 -0600

    Time: 168 ms (68.94%), Space: 15.5 MB (30.79%) - LeetHub

--- move-zeroes/move-zeroes.py
+++ move-zeroes/move-zeroes.py
@@ -8,22 +8,19 @@ class Solution:
         1,0,2,0,4,0,2,3,0 
         
         1,2,4,2,3,0,2,3,0 
-                         l 
-                 r
+                          l 
+                r
         """
         
-        zeroCount = 0
         fastPointer = 0
         slowPointer = 0
         
         for fastPointer in range(0,len(nums)):
             
             if nums[fastPointer]!= 0:
-                nums[slowPointer] = nums[fastPointer]
+                nums[slowPointer],nums[fastPointer] = nums[fastPointer], nums[slowPointer]
                 slowPointer += 1
-        
-        for i in range(slowPointer, len(nums)):
-            nums[i] = 0
+
     
                 
 
