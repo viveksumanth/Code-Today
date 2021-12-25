3943aa66a40df2971e37db09a100869508942583
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2021-12-24 22:32:21 -0600

    Time: 68 ms (91.27%), Space: 16.5 MB (75.46%) - LeetHub

--- minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,34 +1,21 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        
-        # find the subarray whose sum is greater than or equal to the target sum
-        minSize = float('inf')
-        cSum = 0
         pointerA = 0
         pointerB = 0
-        # move pointer b to correct posistion
-        """
-        2 3 1 2 4 3   target 7 minsum = inf cursum = 0
-        csum = 2, 5, 6 8
-        pointer b = 3
-        minsize = 4
-        pointer a = 0
-        
-        2 3 1 2 4 3
-        |     |
-        """
-        for pointerB in range(0,len(nums)):
-            if cSum < target:
-                cSum = cSum + nums[pointerB]
-            
-            while(cSum >= target):
-                minSize = min(minSize , pointerB - pointerA+1)
-                # move pointerA until >= target condition is violated
-                cSum = cSum - nums[pointerA]
-                pointerA += 1
-                
-            
-        if minSize == float('inf'):
+        sumSoFar = 0
+        minmSubArray = float('inf')
+
+        for idx in range(pointerA, len(nums)):
+            sumSoFar += nums[idx]
+
+            while(sumSoFar >= target):
+                minmSubArray = min(minmSubArray, idx - pointerB+ 1)
+                sumSoFar -= nums[pointerB]
+                pointerB = pointerB + 1
+
+        if minmSubArray == float('inf'):
             return 0
-        return minSize
+
+        return minmSubArray
+
         
