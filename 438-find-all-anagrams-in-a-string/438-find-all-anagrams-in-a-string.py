fce551bcd52f85b2754374cccd2330c79111f073
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2022-02-02 00:25:21 -0500

    Time: 236 ms (27.13%), Space: 15.2 MB (41.94%) - LeetHub

---
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,59 @@
+'''
+    |
+    c b a e b a b a c d
+        |
+
+
+'''
+
+
+from collections import Counter
+
+
+class Solution:
+    def findAnagrams(self, string: str, tem: str) -> List[int]:
+        
+        windowSize = len(tem)
+        tem = Counter(tem)
+        result = []
+        newHm = dict()
+        current = 0
+        
+        for eachIdx in range(0,len(string)):
+            
+            if newHm == tem:
+                result.append(abs(windowSize - eachIdx))
+                
+            while(eachIdx - current + 1 > windowSize):
+                letter = string[current]
+                
+                if letter in newHm:
+                    if newHm[letter] > 1:
+                        newHm[letter] -= 1
+                    else:
+                        del newHm[letter]
+                
+                current = current + 1
+            
+            letter = string[eachIdx]
+            
+            if letter in newHm:
+                newHm[letter] += 1
+            
+            else:
+                newHm[letter] = 1
+                
+        if newHm == tem:
+            result.append(abs(windowSize - eachIdx - 1))   
+
+
+            
+        return result
+                
+                
+                
+            
+        
+        
+        
+        
