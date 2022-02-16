73d4617fe4a88f66740208866668c2e5f9359b04
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2022-02-15 19:49:25 -0500

    Time: 116 ms (37.04%), Space: 13.9 MB (98.24%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    
+    def __init__(self):
+        self.allResults = set()
+        
+    def combinationSumHelper(self,nums,target,ps):
+        
+
+        if sum(ps) > target:
+            return
+        
+        if sum(ps) == target:
+            newPs = tuple(ps)
+            self.allResults.add(newPs)
+            return 
+        
+        for eachIdx in range(len(nums)):
+            
+            currentNum = nums[eachIdx]
+            
+            if eachIdx > 0 and nums[eachIdx-1] == nums[eachIdx]:
+                continue
+                
+            self.combinationSumHelper(nums[eachIdx+1::],target,ps + [currentNum])
+            
+        return 
+    
+            
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        candidates.sort()
+        self.combinationSumHelper(candidates,target,[])
+        
+        return self.allResults
