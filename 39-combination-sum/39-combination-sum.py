39bee289a3e97066cf5e19ad07fc6ef81f15217f
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2022-02-16 22:43:46 -0500

    Time: 121 ms (42.31%), Space: 13.9 MB (97.98%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        
+        def dfs(subsolution, start, current,solutions):
+            
+            if current == 0:
+                solutions.append(subsolution[:])
+                return
+    
+            for i in range(start,len(candidates)):
+                
+                if current - candidates[i] >= 0:
+                    
+                    dfs(subsolution + [candidates[i]],i,current-candidates[i],solutions)
+            
+            return solutions
+        
+        return (dfs([],0,target,[]))
+        
+    
+    
+        
