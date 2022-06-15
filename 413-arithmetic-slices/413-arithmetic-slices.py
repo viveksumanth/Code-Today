4da00aee5f3944ea5653de1c482fcdd2dcfa9914
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2022-06-15 16:31:11 -0400

    Time: 42 ms (83.65%), Space: 14.2 MB (46.93%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.py
+++ 413-arithmetic-slices/413-arithmetic-slices.py
@@ -4,22 +4,20 @@ class Solution:
         if len(nums) > 2:
             prev = nums[0] - nums[1]
             
-        count = 1
+        count = 2
         totalSlices = 0
         
         for eachIdx in range(2,len(nums)):
             curr = nums[eachIdx - 1] - nums[eachIdx]
 
             if prev != curr:
-                count += 1
                 totalSlices += ((count-2)*(count-1))//2
                 prev = curr
-                count = 1
-                
+                count = 2
+            
             else: 
                 count += 1
         
-        count += 1
         totalSlices += ((count-2)*(count-1))//2
         
         return totalSlices
