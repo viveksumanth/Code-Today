4cfc17ff6bf1dfc34b2c5ef1f0854633fafc8fe9
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2023-10-19 13:24:28 -0700

    Time: 41 ms (37.23%), Space: 16.3 MB (18.25%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -1,23 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def __init__(self):
-        self.result = []
+class Solution:
+    def moveLeft(self, root):
+        leftNodes = []
+        while(root != None):
+            leftNodes.append(root)
+            root = root.left
+        return leftNodes
         
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        if root == None:
-            return 
-
-        self.inorderTraversal(root.left)
-        self.result.append(root.val)
-        self.inorderTraversal(root.right)
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        if root is None:
+            return result
         
-        return self.result
+        stack = self.moveLeft(root)
+        while(stack):
+            # append stack untill the left is not none
+            currentNode = stack.pop()
+            result.append(currentNode.val)
+            if currentNode.right:
+                rightNode = currentNode.right
+                stack.append(rightNode)
+                getLeftNodes = self.moveLeft(rightNode.left)
+                stack.extend(getLeftNodes)
+        
+        return result
+            
+            
