22ff43b05fbc3688d0c29cb27be58eff9b90feb2
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2023-10-27 00:53:38 -0700

    Time: 223 ms (52.50%), Space: 26.3 MB (23.66%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
@@ -1,21 +1,21 @@
 from collections import Counter
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
-        
-        sumDict = dict()
         pair = 0
+        listOfTuples = [tuple(sorted(inner_list)) for inner_list in dominoes]
+        counterDict = Counter(listOfTuples)
+
+        for each in counterDict:
+            if counterDict[each] > 1:
+                pair += math.comb(counterDict[each], 2)
+                
+        return pair
+        
+        
+        # pair = 0
         # for i in range(0, len(dominoes)):
         #     for j in range(i+1, len(dominoes)):
         #         if (dominoes[i][0] == dominoes[j][0] and dominoes[i][1] == dominoes[j][1]) or (dominoes[i][0] == dominoes[j][1] and dominoes[i][1] == dominoes[j][0]):
         #             pair += 1
-        
-        listOfTuples = [tuple(sorted(inner_list)) for inner_list in dominoes]
-        
-        counterList = Counter(listOfTuples)
-        
-        for each in counterList:
-            if counterList[each] > 1:
-                pair += math.comb(counterList[each], 2)
-                
-        return pair
+        # return pair
         
