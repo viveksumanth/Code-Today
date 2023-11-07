8961506c872a761c459ed7875d1fa7fe5dfc9bd6
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2023-11-07 15:29:14 -0800

    Time: 728 ms (27.81%), Space: 32.3 MB (42.51%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.py
@@ -1,33 +1,18 @@
 class Solution:
     def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
-        heap = []
-        for i in range(len(dist)):
-            heap.append(dist[i] / speed[i])
-            
-        heapq.heapify(heap)
-        ans = 0
-        while heap:
-            if heapq.heappop(heap) <= ans:
+
+        timeToCity = []
+        
+        for i in range(0,len(dist)):
+            timeToCity.append(dist[i] / speed[i])
+        
+        timeToCity.sort()
+        count = 0
+
+        for i in range(len(timeToCity)):
+            if timeToCity[i] <= i:
                 break
+            count += 1
             
-            ans += 1
-            
-        return ans
-        
-#         timeToCity = []
-        
-#         for i in range(0,len(dist)):
-#             timeToCity.append(dist[i] // speed[i])
-        
-#         timeToCity.sort()
-#         print(timeToCity)
-#         count = 0
-        
-#         for i in range(1,len(timeToCity)):
-#             if timeToCity[i-1] == timeToCity[i]:
-#                 return count + 1
-#             else:
-#                 count += 1
-        
-#         return count
+        return count
         
