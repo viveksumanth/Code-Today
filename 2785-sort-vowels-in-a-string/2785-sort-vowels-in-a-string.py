cc2f217826497f28c744df1f0d267889223ec095
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2023-11-13 11:42:17 -0800

    Time: 185 ms (34.57%), Space: 18.1 MB (98.14%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -2,18 +2,27 @@ class Solution:
     def sortVowels(self, s: str) -> str:
         sList = list(s)
         vowelsInString = list()
-        vowels = ("a", "A", "e", "E", "i", "I", "O", "o", "U", "u")
+        vowels = "AEIOUaeiou"
+        vowelsLookup = {
+            0:"A", 1:"E", 2:"I", 3:"O", 4:"U",5:"a", 6:"e", 7:"i", 8:"o", 9:"u"
+        }
+        vowelCount = [0]*10
         
         for eachChar in sList:
             if eachChar in vowels:
-                vowelsInString.append(eachChar)
-        
-        vowelsInString.sort()
-        curChar = 0
+                index = vowels.index(eachChar)
+                vowelCount[index] += 1
+                
+        pointerA = 0
         for eachCharIdx in range(0, len(sList)):
             if sList[eachCharIdx] in vowels:
-                sList[eachCharIdx] = vowelsInString[curChar]
-                curChar += 1
+                while(pointerA < len(vowelCount)):
+                    if vowelCount[pointerA] != 0:
+                        charecter = vowelsLookup[pointerA]
+                        sList[eachCharIdx] = charecter
+                        vowelCount[pointerA] -= 1
+                        break
+                    pointerA += 1
         
         return "".join(sList)
         
