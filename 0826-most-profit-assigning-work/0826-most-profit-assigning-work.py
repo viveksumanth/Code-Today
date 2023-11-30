7e8b043a5cdcaad8e599ff833aa5a3295a687d89
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2023-11-30 00:22:25 -0800

    Time: 416 ms (18.50%), Space: 19.6 MB (16.99%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,8 +1,10 @@
 class Solution:
     def getClosestJob(self, worker, difficulty, left, right):
         closest = left
+        
         while(left <= right):
             mid = (left + right)//2
+            
             if difficulty[mid] > worker:
                 right = mid-1
             else:
@@ -30,13 +32,14 @@ class Solution:
         28 -> closest - 18
         18 -> closest - 18
         
-        
+        Think about the repeated difficulties and profits. 
         
         '''
 
         lookup = dict()
         lookup[0] = 0
         
+        #Prepare Data
         for i in range(0,len(difficulty)):
             if difficulty[i] in lookup:
                 lookup[difficulty[i]] = max(lookup[difficulty[i]], profit[i])
@@ -44,14 +47,17 @@ class Solution:
                 lookup[difficulty[i]] = profit[i]
 
         maxValue = float("-inf")
+        # sort for the binary search
         difficulty.sort()
         
+        # setup the maximum profit for a job
         for each in difficulty:
             profitValue = lookup[each]
             maxValue = max(profitValue, maxValue)
             if maxValue > profitValue:
                 lookup[each] = maxValue
-
+        
+        # get closest difficulty (maximum) for the worker's ability
         result = 0
         for eachWorker in worker:
             closestJob = self.getClosestJob(eachWorker, 
@@ -59,4 +65,5 @@ class Solution:
                                        left = 0, 
                                        right = len(difficulty)-1)
             result += lookup[closestJob]
+            
         return result
