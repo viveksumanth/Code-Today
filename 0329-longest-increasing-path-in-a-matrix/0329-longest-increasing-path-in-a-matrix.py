b744a0386459dd700094cf8f5ff5f5a03d632987
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2023-12-04 20:38:06 -0800

    Time: 584 ms (7.57%), Space: 17.8 MB (67.22%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -11,7 +11,8 @@ class Solution:
             
             if 0 <= newRow < len(matrix) and 0 <= newCol < len(matrix[0]):
                 if matrix[newRow][newCol] > matrix[row][col]:
-                    neighbours.append(tuple([newRow, newCol]))  
+                    neighbours.append(tuple([newRow, newCol]))
+                    
         return neighbours
     
     def dfs(self, matrix, coordinate, memo):
@@ -19,16 +20,14 @@ class Solution:
             return memo[coordinate]
         
         neighbours = self.getNeighbours(matrix, coordinate)
-        
         if len(neighbours) == 0:
             return 1
         
         tempPath = float("-inf")
         for each in neighbours:  
             tempPath = max(tempPath, self.dfs(matrix, each, memo)+1)
-                
         memo[coordinate] = tempPath
-        return tempPath
+        return memo[coordinate]
         
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
         if len(matrix) == 0:
@@ -40,11 +39,9 @@ class Solution:
         for i in range(0,len(matrix)):
             for j in range(0, len(matrix[0])):
                 coordinate = tuple([i,j])
-                visited = set()
-                visited.add(coordinate)
                 longestPath = max(longestPath, 
-                                  self.dfs(matrix, coordinate, memo))
-                # print(memo)
-                # print("-----------------------")
+                                  self.dfs(matrix, 
+                                           coordinate, 
+                                           memo))
         return longestPath
     
