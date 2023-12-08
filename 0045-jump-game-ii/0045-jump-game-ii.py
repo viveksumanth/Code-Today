52266570cc251118c005b8190837976ca9e5616d
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2023-12-08 15:59:34 -0800

    Time: 120 ms (59.76%), Space: 17.5 MB (14.27%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        left = 0
+        right = 0
+        result = 0
+        
+        while(right < len(nums)-1):
+            farthest = 0
+            for i in range(left, right+ 1):
+                farthest = max(farthest, i+nums[i])
+                
+            left = right+1
+            right = farthest
+            result += 1
+        
+        return result
