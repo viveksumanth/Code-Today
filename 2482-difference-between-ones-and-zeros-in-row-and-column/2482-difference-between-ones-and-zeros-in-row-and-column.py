e501c659c0bf62d20ef00984553acec7d1b727aa
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2023-12-13 18:10:43 -0800

    Time: 1500 ms (15.72%), Space: 56.7 MB (21.43%) - LeetHub

---
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -0,0 +1,52 @@
+class Solution:
+    def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
+        '''
+        rowLookup = {
+        0:[1,2]
+        1:[1,2]
+        2:[2,1]
+        }
+        
+        colLookup:{
+        0:[2,1]
+        1:[2,1]
+        2:[0,3]
+        }
+        
+        total = 
+        '''
+        rowLookup = list()
+        colLookup = list()
+        rows = len(grid)
+        cols = len(grid[0])
+        
+        for i in range(0,rows):
+            zeroCount = oneCount = 0
+            for j in range(0,cols):
+                if grid[i][j] == 0:
+                    zeroCount += 1
+                else:
+                    oneCount += 1
+            
+            rowLookup.append([zeroCount, oneCount])
+        
+        for i in range(0,cols):
+            zeroCount = oneCount = 0
+            for j in range(0,rows):
+                if grid[j][i] == 0:
+                    zeroCount += 1
+                else:
+                    oneCount += 1
+                    
+            colLookup.append([zeroCount, oneCount])
+
+        result = [[0 for _ in range(cols)] for _ in range(rows)]
+        
+        
+        for i in range(len(rowLookup)):
+            for j in range(len(colLookup)):
+                result[i][j] = rowLookup[i][1]  + colLookup[j][1] - rowLookup[i][0] - colLookup[j][0]
+        return result
+    
+    
+        
