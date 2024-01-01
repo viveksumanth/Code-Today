9752ddd1762f29bca34cd5e644d27baf6638793d
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2024-01-01 10:16:42 -0800

    Time: 232 ms (11.06%), Space: 19.2 MB (13.41%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.py
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -16,9 +16,7 @@ class Solution:
         while(gPointer < len(g) and sPointer < len(s)):
             if s[sPointer] >= g[gPointer]:
                 child += 1
-                sPointer += 1
                 gPointer += 1
-            else:
-                sPointer += 1
+            sPointer += 1
     
         return child
