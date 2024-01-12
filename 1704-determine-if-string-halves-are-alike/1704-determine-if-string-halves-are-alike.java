f87ea81a37d11c19a951cff0dbbb51898f3fbb82
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2024-01-11 16:29:41 -0800

    Time: 5 ms (51.60%), Space: 41.8 MB (24.13%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,31 +1,33 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        vowels.add('A');
+        vowels.add('E');
+        vowels.add('I');
+        vowels.add('O');
+        vowels.add('U');
         
-        Set<Character> vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        int midIndex = s.length()/2;
+        int leftVowelCount = 0;
+        int rightVowelCount = 0;
         
-        // divide the string into 2 halfs and count number of vowels
-        int strLength = s.length();
-        int firstHalf = 0;
-        int secondHalf = 0;
-        
-        for(int idx = 0; idx<strLength;idx++){
-            
-            if(idx < (strLength/2)){
-                //count vowels
-                if(vowels.contains(s.charAt(idx))){
-                     firstHalf += 1;
-                }
-            }else
-                if(vowels.contains(s.charAt(idx))){
-                    secondHalf += 1;
-                    
-                }
+        for(int i=0; i<midIndex; i++) {
+            if(vowels.contains(s.charAt(i))) {
+                leftVowelCount++;
+            }
         }
-
-        if(firstHalf == secondHalf){
-            return true;
-        };
         
-        return false;
+        for (int i=midIndex; i< s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                rightVowelCount++;
+            }
+        }
+        
+        return leftVowelCount == rightVowelCount;
     }
 }
