69dea13562169fc78fd42eb7286cf4bac0594c23
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2024-01-17 11:10:24 -0800

    Time: 4 ms (15.75%), Space: 41.3 MB (54.36%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -2,24 +2,12 @@ class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         Map<Integer, Integer> lookup = new HashMap<>();
         Set<Integer> uniqueValues = new HashSet<>();
-        
-        for (int eachNum:arr) {
-            if(lookup.containsKey(eachNum)){
-                lookup.put(eachNum, lookup.get(eachNum)+1);
-            } else {
-                lookup.put(eachNum, 1);
-            }
+
+        for (int eachNum : arr) {
+            lookup.merge(eachNum, 1, Integer::sum);
         }
-        
-        for (Map.Entry<Integer, Integer> entry: lookup.entrySet()) {
-            int value = entry.getValue();
-            if (uniqueValues.contains(value)) {
-                return false;
-            } else {
-                uniqueValues.add(value);
-            }
-        }
-        
-        return true;
+
+        return lookup.values().stream()
+                .noneMatch(value -> !uniqueValues.add(value));
     }
 }
