1ded9c364f3555b5e232ad69395310d5113be641
Vivek Sumanth <61607315+viveksumanth@users.noreply.github.com>
2024-02-09 11:58:37 -0800

    Time: 26 ms (97.53%), Space: 16.6 MB (59.70%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isValid(self, string: str) -> bool:
+        '''
+        { '(' : ')', '{' :'}', '[' : ']' }
+        
+        [({)}]
+        
+        [ ] ) }]
+        
+        
+        when stack is empty then string is balanced
+        
+        edge cases:
+            what if stack is empty.
+            what if element is left in stack.
+        
+        '''
+        
+        stack = []
+        
+        lookup = { '(' : ')', '{' :'}', '[' : ']' }
+        
+        closingBrackets = ')}]'
+        
+        for bracket in string:
+            
+            if bracket in lookup: #check opening bracket and append closing bracket
+                
+                stack.append(lookup[bracket])
+            
+            elif bracket in closingBrackets: # check if bracket in closing bracket
+                
+                # if last element in bracket is not equal to bracket that means we encouterd wrong
+                # sequence
+                if stack and stack[-1] == bracket:
+                    stack.pop()
+                else:
+                    return False
+        
+        if len(stack):
+            return False
+        
+        return True
